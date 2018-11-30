859456506c04c74b35eeec7a6edd8e3388ab68fd
vikaskumar <vikaskumar@policybazaar.com>
2018-11-30 11:36:19 +0530

    code refactor

--- multer-s3-fileupload/s3UploadClient.js
+++ multer-s3-fileupload/s3UploadClient.js
@@ -1,28 +1,31 @@
-const aws = require('aws-sdk')
-const multer = require('multer')
-const multerS3 = require('multer-s3')
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
 
 const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_DEFAULT_REGION
+  // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  // region: process.env.AWS_DEFAULT_REGION
+  accessKeyId: 'AKIAIYMEYSRN6XIZP22Q',
+  secretAccessKey: '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',
+  region: 'ap-south-1'
 })
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: process.env.AWS_S3_BUCKET,
+    bucket: 'cctecbuckt',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: 'public-read',
     metadata: (req, file, cb) => {
-      cb(null, { fieldName: file.fieldname })
+      cb(null, { fieldName: file.fieldname });
     },
     key: (req, file, cb) => {
-      cb(null, 'files_from_node/' + Date.now().toString() + file.originalname)
+      cb(null, 'files_from_node/' + Date.now().toString() + file.originalname);
     }
   })
-})
+});
 
 module.exports = {
   upload
-}
+};
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const server = express()
+const server = express();
 
 // Ensure that S3 Bucket is properly loaded
-console.log('S3 BUCKET', process.env.AWS_S3_BUCKET)
+console.log('S3 BUCKET', 'cctecbuckt');
 
 // Middleware Plugins
-server.use(bodyParser.json())
-server.use(express.static('public')) // Just for testing, use a static html
-
+server.use(bodyParser.json());
+server.use(express.static('public'));
 // Routes
 server.use('/', [
   require('./routes/fileupload')
-])
+]);
 
 
 // Start the server
 server.listen(7000, error => {
   if (error) console.error('Error starting', error)
-  else console.log('Started at http://localhost:7000')
-})
+  else console.log('Started at http://localhost:7000');
+});
